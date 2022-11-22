bd6c5b6bc6dc51052d59b97096391844ee9c1077
Ryan <stander.ryan@gmail.com>
2022-11-22 15:02:26 +0100

    star auto adjust

---
+++ Therapeut Vechter/Assets/Scripts/UI/StarPositionSetter.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace UI
+{
+    public class StarPositionSetter : MonoBehaviour
+    {
+        [SerializeField] private RectTransform bottomStar;
+        [SerializeField] private RectTransform middleStar;
+        [SerializeField] private RectTransform topStar;
+        [SerializeField] private RectTransform scoreSlider;
+        private void OnValidate()
+        {
+            var height = scoreSlider.rect.width;
+
+            var bottomStarRect = bottomStar.localPosition;
+            bottomStarRect.x =  height* 1/3 - height/2;
+            bottomStar.localPosition = bottomStarRect;
+            
+            var middleStarRect = middleStar.localPosition;
+            middleStarRect.x = height* 2/3 - height/2;
+            middleStar.localPosition = middleStarRect;
+            
+            var topStarRect = topStar.localPosition;
+            topStarRect.x = height - height/2;
+            topStar.localPosition = topStarRect;
+
+        }
+    }
+}
