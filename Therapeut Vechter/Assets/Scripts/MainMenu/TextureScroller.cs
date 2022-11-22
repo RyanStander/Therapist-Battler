7f3257046a5f775cb7f15092cf7f0051ac15d8ad
LucRoosjen <luc.roosjen@gmail.com>
2022-11-22 10:37:38 +0100

    prefabs of ingame Ui, images for roadmap

--- Therapeut Vechter/Assets/Scripts/MainMenu/TextureScroller.cs
+++ Therapeut Vechter/Assets/Scripts/MainMenu/TextureScroller.cs
@@ -1,34 +1,23 @@
 using UnityEngine;
-using Unity.Collections;
+using UnityEngine.UI;
+
 
 namespace MainMenu
 {
     public class TextureScroller : MonoBehaviour
     {
-       // [SerializeField] private float scrollX;
-        //[SerializeField] private float scrollY;
-        public int materialIndex = 0;
-        public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
-        public string textureName = "_MainTex";
-
-        Vector2 uvOffset = Vector2.zero;
-//https://answers.unity.com/questions/19848/making-textures-scroll-animate-textures.html
-        void LateUpdate()
+        [SerializeField] private RawImage scrollImage;
+        [SerializeField] private float scrollX;
+        [SerializeField] private float scrollY;
+        [SerializeField] private Image maskImage;
+        [SerializeField] private Image parentImage;
+        
+        void Update()
         {
-            uvOffset += (uvAnimationRate * Time.deltaTime);
-            if (GetComponent<Renderer>().enabled)
-            {
-                GetComponent<Renderer>().materials[materialIndex].SetTextureOffset(textureName, uvOffset);
-            }
+            scrollImage.uvRect = new Rect(scrollImage.uvRect.position + new Vector2(scrollX, scrollY) * Time.deltaTime,
+                scrollImage.uvRect.size);
+            maskImage.fillAmount = parentImage.fillAmount;
+            
         }
     }
-
-/*
-    // Update is called once per frame
-    void Update()
-    {
-        float offsetX = Time.time * scrollX;
-        float offsetY = Time.time * scrollY;
-        GetComponent<Image>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
-    }*/
 }
